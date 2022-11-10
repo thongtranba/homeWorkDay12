233993f8693bd1e852babab97804e780803ef342
Ba Thong Tran <82447776+thongtranba@users.noreply.github.com>
2022-11-10 19:49:33 +0100

    Add files via upload
    update minimumgap

--- src/homeWorkDay12/MininumGap.java
+++ src/homeWorkDay12/MininumGap.java
@@ -7,28 +7,26 @@ import java.util.List;
 
 public class MininumGap {
 	public static void main(String[] args) {
-		List<Integer> array = new ArrayList<Integer>(Arrays.asList(7,1,3,4,1,7));
+		List<Integer> array = new ArrayList<Integer>(Arrays.asList(7, 1, 3, 4, 1, 7));
 		int result = minimumGap(array);
 		System.out.println("the mininum gap: " + result);
 	}
 
 	public static int minimumGap(List<Integer> arr) {
 		int size = Collections.max(arr);
-		List<Integer> pairs = new ArrayList<Integer>(Arrays.asList(new Integer[size+1]));
-
-		
+		List<Integer> pairs = new ArrayList<Integer>(Arrays.asList(new Integer[size + 1]));
+		List<Integer> minGaps = new ArrayList<Integer>();
 		int currIndex = 0;
 		int prevIndex = 0;
-		int minGap = -1;
-
 		for (int i = 0; i < arr.size(); i++) {
 			if (pairs.contains(arr.get(i))) {
 				currIndex = i;
 				prevIndex = pairs.get(arr.get(i));
-				minGap = (currIndex-prevIndex) ;
+			
+				minGaps.add(currIndex - prevIndex);
 			}
 			pairs.set(arr.get(i), i);
 		}
-		return minGap ;
+		return Collections.min(minGaps);
 	}
 }
