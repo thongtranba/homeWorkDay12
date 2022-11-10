e1cc89d2ca8bfc10ea28c51652e0e9f6fa65d8e0
Ba Thong Tran <82447776+thongtranba@users.noreply.github.com>
2022-11-10 15:28:55 +0100

    Add files via upload
    the feast

--- src/homeWorkDay12/TheFeast.java
+++ src/homeWorkDay12/TheFeast.java
@@ -1,6 +1,21 @@
 package homeWorkDay12;
 
 public class TheFeast {
-	
+	public static void main(String[] args) {
+		int result = feast(12, 4, 4);
+		System.out.println("The Feast: " + result);
+
+	}
+
+	public static int feast(int money, int cost, int exchange) {
+		int numOfChocolate = money / cost;
+		int wrapper = numOfChocolate;
+
+		while (wrapper >= numOfChocolate) {
+			numOfChocolate = numOfChocolate + 1;
+			wrapper = wrapper - exchange + 1;
+		}
+		return numOfChocolate;
+	}
 
 }
